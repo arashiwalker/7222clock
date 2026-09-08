bf288f9eb4c72b8b09db45f7cfb6001c5432ad50
arashi walker <arashiwalker@gmail.com>
2026-09-08 04:33:46 -0700

    Bump service worker to v4 after Pay button fix.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = '7222-clock-v3';
+﻿const CACHE_NAME = '7222-clock-v4';
 const FILES_TO_CACHE = [
   '/',
   '/index.html',
@@ -42,3 +42,4 @@ self.addEventListener('fetch', (event) => {
     caches.match(req).then((response) => response || fetch(req))
   );
 });
+
